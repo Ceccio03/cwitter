ad700b478a01212879c7620731aacb8cdf198ee8
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-10-11 13:22:34 +0200

    new repo

---
+++ src/app/firebase-config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDMnUU5yQWLbaB80IxVQh22WtKMxsgP1ZM",
+    authDomain: "cwitter-9681e.firebaseapp.com",
+    projectId: "cwitter-9681e",
+    storageBucket: "cwitter-9681e.appspot.com",
+    messagingSenderId: "774923280391",
+    appId: "1:774923280391:web:6281f6e445bfa75e058aa2"
+};
