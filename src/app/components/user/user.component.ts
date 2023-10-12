840b806fcafaa643e5edfc9c4ada7c44bfeb7f27
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-10-12 09:49:49 +0200

    .

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 @Component({
   selector: 'app-user',
@@ -11,18 +10,6 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 })
 export class UserComponent implements OnInit {
   ngOnInit(): void {
-    const auth = getAuth();
     
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed up 
-        const user = userCredential.user;
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-      });
   }
 }
